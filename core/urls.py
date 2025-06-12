21b07073d8867e07a82ee12831df276ec90d85ed
micho8cho93 <182514846+micho8cho93@users.noreply.github.com>
2025-06-12 13:03:28 +0200

    added view, url, template for todoitem model

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from . import views
 from .views import home
 
+app_name = 'core'
 urlpatterns = [
     path('', home),
+    path("list", views.IndexView.as_view(), name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from django.views import generic
+from django.utils import timezone
+
+from .models import TodoItem
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+class IndexView(generic.ListView):
+    template_name = "todos/index.html"
+    context_object_name = "todo_list"
+
+    def get_queryset(self):
+        """
+        Return the list of todo items, ordered by creation date.
+        """
+        return TodoItem.objects.order_by('-created_at')
+    def get_context_data(self, **kwargs):
+        """
+        Add additional context data to the template.
+        """
+        context = super().get_context_data(**kwargs)
+        context['current_date'] = timezone.now().date()
+        return context
